b02c05a74648327af2faf9cf410df2cf522a3580
Aarthi S <aarthia776@gmail.com>
2025-02-01 09:18:09 +0530

    Add files via upload

---
+++ pizza-website/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const orderButton = document.querySelector(".order-now");
+    
+    orderButton.addEventListener("click", function() {
+        alert("Thank you for ordering! Your delicious pizza will be ready soon.");
+    });
+});
