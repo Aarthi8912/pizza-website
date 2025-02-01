b88d93a2e9a98e8d9aa8a65055b7e227aab08c8b
Aarthi S <aarthia776@gmail.com>
2025-02-01 09:47:13 +0530

    Delete pizza-website directory

--- pizza-website/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const orderButton = document.querySelector(".order-now");
-    
-    orderButton.addEventListener("click", function() {
-        alert("Thank you for ordering! Your delicious pizza will be ready soon.");
-    });
-});
